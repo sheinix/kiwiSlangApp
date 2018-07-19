9d4988b57f2ee45ee7397185322c4eabf0ee7f22
Juan Nuvreni <jnuvreni@gmail.com>
2018-07-19 15:15:49 -0500

    initial commit =)

---
+++ kiwiSlangApp/Pods/Target Support Files/Pods-kiwiSlangAppTests/Pods-kiwiSlangAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kiwiSlangAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_kiwiSlangAppTests
+@end
---
+++ kiwiSlangApp/kiwiSlangApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kiwiSlangApp
+//
+//  Created by Juan Nuvreni on 19/07/2018.
+//  Copyright © 2018 sheinix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
